e7ae8f3775bbec454c9e631cfec0105d158c68ed
Ryell Martinez <ryell.martinez@student.tdsb.on.ca>
2024-11-18 11:35:56 -0500

    added basic header html

---
+++ src/components/headerView.jsx
@@ -0,0 +1,13 @@
+import "./headerView.css";
+function headerView(){
+return(
+    <div class="tb-item top-item">
+<div class="buttons">
+    <button>Sign Up</button>
+    <button>Sign In</button>
+</div>
+<h class="logo">WacFlix</h>
+</div>
+)
+}
+export default headerView;
