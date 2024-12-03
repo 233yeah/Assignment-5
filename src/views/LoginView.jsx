b79599c6ed14808425ef752ed79506ba5412cdfe
Ryell Martinez <ryell.martinez@student.tdsb.on.ca>
2024-12-03 11:40:29 -0500

    added password

--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -9,8 +9,14 @@ function LoginView() {
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
 
-    function genrePage() {
-        navigate(`/movie/genre/28`);
+    function genrePage(event) {
+        event.preventDefault();
+        console.log(password);
+        if (password === "keshi") {
+            navigate(`/movie/genre/28`);
+        } else {
+          alert("Wrong password!");
+        }
     }
 
     return (
@@ -18,12 +24,12 @@ function LoginView() {
             <Header />
             <div className="login-container">
                 <h className="login-title">Welcome Back</h>
-                <form className="login-form" onSubmit={(event) => { login(event) }}>
+                <form className="login-form" onSubmit={(event) => { genrePage(event) }}>
                     <label className="login-text">Email:</label>
-                    <input type="text" className="login-inputs" value={email} onChange={(event) => { setEmail(event.target.value) }} required />
+                    <input type="email" className="login-inputs" value={email} onChange={(event) => { setEmail(event.target.value) }} required />
                     <label className="login-text">Password:</label>
-                    <input type="text" className="login-inputs" value={password} onChange={(event) => { setPassword(event.target.value) }} required />
-                    <button className="login-button" onClick={() => { genrePage() }}>Sign In</button>
+                    <input type="password" className="login-inputs" value={password} onChange={(event) => { setPassword(event.target.value) }} required />
+                    <button className="login-button" type="submit">Sign In</button>
                 </form>
             </div>
             <Footer />
